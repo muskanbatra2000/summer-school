bbaac5f7ead353415cae6534a0563ec737dd842e
muskan <muskanbatra2000@gmail.com>
2019-02-27 20:05:17 +0530

    muskan2.c

---
+++ lab7.c
@@ -0,0 +1,39 @@
+
+#include<stdio.h>
+{
+	int i,n;	printf("print all even number");
+	scanf("%d", &n);
+	printf("
+			#include<stdio.h>
+
+void main()
+
+{
+    int a=1,n,sum;
+
+
+
+   printf("enter the value of n");
+
+  scanf("%d",&n);
+
+ while(a<=n)
+
+ {
+
+     sum=sum+a;
+
+
+        a++;
+
+
+    }
+
+
+
+    printf("the total sum is%d",sum);
+
+return 0;
+
+}
+
