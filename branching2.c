acbb164c000ddc4d703f84955a20fbac1a9756c4
muskan <muskanbatra2000@gmail.com>
2019-03-07 10:28:06 +0530

    https://github.com/muskanbatra2000/muskan-5

--- branching2.c
+++ branching2.c
@@ -9,31 +9,13 @@ class complex
 		{
 			cin>>re>>im;
 		}
-		void disp()
-		{
-			cout<<re<<"+"<<im<<"i";
-
-		}
-		void sum(complex,complex);
-};
-void complex::sum(complex c1,complex c2)
-{
-	re=c1.re+c2.re;
-	im=c1.im+c2.im;
-}
+		
 int main()
 {
 	complex c1,c2,c3;
 	cout<<"Enter 1st complex no.:";
 	c1.get();
 	cout<<"Enter 2nd complex no.:";
-	c2.get();
-	cout<<"The 1st complex no. is";
-	c1.disp();
-	cout<<"\nThe 2nd complex no. is";
-	c2.disp();
-	c3.sum(c1,c2);
-	cout<<"\nThe resultant complex no. is";
-	c3.disp();
+
 
 }
